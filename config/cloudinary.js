065461977d4a44a766e3c43d133d0eef97e41265
yaqoob-ptg <yaqoob.ptg@gmail.com>
2026-04-15 14:36:48 +0500

    multer-storage-cloudinary issue fix

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -38,4 +38,4 @@ cloudinary.config({
 // });
 
 
-module.exports = { cloudinary, };
+module.exports =  cloudinary ;
